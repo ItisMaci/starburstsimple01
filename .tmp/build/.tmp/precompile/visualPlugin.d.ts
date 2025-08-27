88c6bc783699bf7123c31410a923d3da13d9b617
Marcel <if24b082@technikum-wien.at>
2025-08-27 08:38:37 +0200

    Package creation test

---
+++ .tmp/build/.tmp/precompile/visualPlugin.d.ts
@@ -0,0 +1,4 @@
+import powerbiVisualsApi from "powerbi-visuals-api";
+import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
+declare var starburstsimple0195D57F49CDC347B7B59E21750C972DF4: IVisualPlugin;
+export default starburstsimple0195D57F49CDC347B7B59E21750C972DF4;
---
+++ .tmp/precompile/visualPlugin.ts
@@ -0,0 +1,32 @@
+import { Visual } from "../../src/visual";
+import powerbiVisualsApi from "powerbi-visuals-api";
+import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
+import VisualConstructorOptions = powerbiVisualsApi.extensibility.visual.VisualConstructorOptions;
+import DialogConstructorOptions = powerbiVisualsApi.extensibility.visual.DialogConstructorOptions;
+var powerbiKey: any = "powerbi";
+var powerbi: any = window[powerbiKey];
+var starburstsimple0195D57F49CDC347B7B59E21750C972DF4: IVisualPlugin = {
+    name: 'starburstsimple0195D57F49CDC347B7B59E21750C972DF4',
+    displayName: 'starburst_simple_01',
+    class: 'Visual',
+    apiVersion: '5.3.0',
+    create: (options?: VisualConstructorOptions) => {
+        if (Visual) {
+            return new Visual(options);
+        }
+        throw 'Visual instance not found';
+    },
+    createModalDialog: (dialogId: string, options: DialogConstructorOptions, initialState: object) => {
+        const dialogRegistry = (<any>globalThis).dialogRegistry;
+        if (dialogId in dialogRegistry) {
+            new dialogRegistry[dialogId](options, initialState);
+        }
+    },
+    custom: true
+};
+if (typeof powerbi !== "undefined") {
+    powerbi.visuals = powerbi.visuals || {};
+    powerbi.visuals.plugins = powerbi.visuals.plugins || {};
+    powerbi.visuals.plugins["starburstsimple0195D57F49CDC347B7B59E21750C972DF4"] = starburstsimple0195D57F49CDC347B7B59E21750C972DF4;
+}
+export default starburstsimple0195D57F49CDC347B7B59E21750C972DF4;
